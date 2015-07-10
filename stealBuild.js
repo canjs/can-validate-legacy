4a7de30a12c27e7db992ac2bfcdb55e94e61c17a
Juan Orozco <juanthedesigner@gmail.com>
2015-07-10 12:51:19 -0400

    Fixed paths for browserify environments.

--- stealBuild.js
+++ stealBuild.js
@@ -3,7 +3,7 @@ var stealTools = require("steal-tools");
 stealTools.export({
 	system: {
 		config: "package.json!npm",
-		main: "build-validate"
+		main: "can-validate.build"
 	},
 	options: {
 		sourceMaps: true
