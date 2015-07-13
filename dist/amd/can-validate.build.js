608ee0cefdc161ecdf186980738952c86c937981
Juan Orozco <juanthedesigner@gmail.com>
2015-07-13 15:49:20 -0400

    Not supposed to have dist in git.

--- dist/amd/can-validate.build.js
+++
@@ -1,9 +0,0 @@
-/*can-validate.build*/
-define(function (require, exports, module) {
-    module.exports = {
-        'can-validate': { 'can-validate': require('./can-validate/can-validate') },
-        map: { validate: require('./can-validate/map/validate/validate') },
-        shims: { 'validatejs.shim': require('./can-validate/shims/validatejs.shim') }
-    };
-});
-//# sourceMappingURL=can-validate.build.js.map
--- dist/cjs/can-validate.build.js
+++
@@ -1,7 +0,0 @@
-/*can-validate.build*/
-module.exports = {
-    'can-validate': { 'can-validate': require('./can-validate/can-validate.js') },
-    map: { validate: require('./can-validate/map/validate/validate.js') },
-    shims: { 'validatejs.shim': require('./can-validate/shims/validatejs.shim.js') }
-};
-//# sourceMappingURL=can-validate.build.js.map
