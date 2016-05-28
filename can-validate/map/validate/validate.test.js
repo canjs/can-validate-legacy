544a2b7588c8ae48a6f150afd3d7cea31f81398d
Juan Orozco <hi@juanorozco.com>
2016-05-28 09:24:45 -0700

    Updated tests to use mocha. Added missing test.

--- can-validate/map/validate/validate.test.js
+++ can-validate/map/validate/validate.test.js
@@ -1,27 +1,71 @@
 /* jshint asi: false */
 import can from 'can';
 import 'can/map/define/';
-import 'can-validate/can-validate';
+import 'can-validate';
 import 'can-validate/map/validate/';
 import 'can-validate/shims/validatejs.shim';
-
-import 'steal-qunit';
-
+import 'chai';
+import 'steal-mocha';
+var expect = chai.expect;
+var validatedMap;
+var secondaryMap;
 var ValidatedMap = can.Map.extend({
 	define: {
 		myNumber: {
 			value: 100,
 			validate: {
-				presence: true
+				required: true
 			}
+		},
+		computedProp: {
+			value: '',
+			validate: {
+				presence: function () {
+					return this.attr('isRequired');
+				}
+			}
+		},
+		isRequired: {
+			value: false,
+			type: 'boolean'
 		}
 	}
 });
 
-var validatedMap = new ValidatedMap();
+describe('Validate can.Map define plugin', function () {
+	describe('on init', function () {
+		describe('validateOnInit is not set', function () {
+			beforeEach(function () {
+				validatedMap = new ValidatedMap();
+			});
+			it('does not validate', function () {
+				expect(can.isEmptyObject(validatedMap.errors)).to.equal(true);
+			});
+		});
+	});
 
-test('validate on set', function () {
-	validatedMap.attr('myNumber', '');
+	describe('when set is called', function () {
+		beforeEach(function () {
+			validatedMap = new ValidatedMap();
+			validatedMap.attr('myNumber', '');
+		});
 
-	equal(validatedMap.errors.myNumber.length, 1, 'Validate ran successfully on set.');
+		it('validates', function () {
+			expect(validatedMap.errors.myNumber.length).to.equal(1);
+		});
+	});
+
+	describe('when creating multiple instances of the same map', function () {
+		beforeEach(function () {
+			validatedMap = new ValidatedMap();
+			secondaryMap = new ValidatedMap({isRequired: true});
+		});
+		it('first map validates successfully', function () {
+			expect(can.isEmptyObject(validatedMap.errors.computedProp)).to.equal(true);
+		});
+		it('second map validates, sets error', function () {
+			expect(secondaryMap.attr('computedProp')).to.equal(true);
+			expect(can.isEmptyObject(secondaryMap.errors.computedProp)).to.equal(true);
+		});
+	});
 });
