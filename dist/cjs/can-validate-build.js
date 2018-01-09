6b6edcafc949624f1ad611f1def006c73504bf0c
Chasen Le Hara <chasen@chasenlehara.com>
2018-01-09 12:45:14 -0800

    1.2.0

---
+++ dist/cjs/can-validate-build.js
@@ -0,0 +1,10 @@
+/*can-validate-build*/
+var canValidate = require('./can-validate.js');
+var validate = require('./map/validate/validate.js');
+var validateJsShim = require('./shims/validatejs.js');
+module.exports = {
+    'can-validate': canValidate,
+    'map': { validate: validate },
+    'shims': { 'validatejs.shim': validateJsShim }
+};
+//# sourceMappingURL=can-validate-build.js.map
